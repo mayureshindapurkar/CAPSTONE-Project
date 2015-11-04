bd202c0bf25836379f0ca9153fb8c65db7a6711b
Shashank Keshava <shashank.keshava@bookmyshow.com>
2015-11-04 16:27:47 +0530

    Created Color Pattern Game

---
+++ html-css-js/course3/dom_colorful_parttern.js
@@ -0,0 +1,20 @@
+function show_pattern() {
+
+	var top_position = 25, left_position = 25;
+	var width = 300, height = 300;
+	var color_list = ["red", "orange", "yellow", "pink", "green", "blue", "indigo", "magenta"];
+	while (width > 50) {
+		var this_div = document.createElement("div");
+		var random_color= Math.floor(Math.random()*color_list.length)+1;
+		this_div.style.top = top_position + "px";
+        this_div.style.left = left_position + "px";
+        this_div.style.width = width + "px";
+        this_div.style.height = height + "px";
+        this_div.style.background = color_list[random_color];
+        the_body.appendChild(this_div);
+        top_position += 10; left_position += 10;
+        width -= 20; height -= 20;
+		
+
+	}
+}
