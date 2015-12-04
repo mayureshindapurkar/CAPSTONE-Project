b44aa2683ec2f5003df5b032e9e3020253b5104f
Shashank Keshava <shashank.keshava@bookmyshow.com>
2015-12-04 11:19:07 +0530

    Updated Solve.js

--- front-end-UI-framework/node-examples/solve.js
+++ front-end-UI-framework/node-examples/solve.js
@@ -1,4 +1,4 @@
-var quad = require('./quadratic');
+/*var quad = require('./quadratic');
 
 quad(1, 4, 1, function(err, quadsolve) {
     if (err) {
@@ -6,4 +6,27 @@ quad(1, 4, 1, function(err, quadsolve) {
     } else {
         console.log("Roots are " + quadsolve.root1() + "  " + quadsolve.root2());
     }
+});*/
+
+
+var quad = require('./quadratic');
+
+var prompt = require('prompt');
+
+prompt.get(['a', 'b', 'c'], function(err, result) {
+    if (err) {
+        return onErr(err);
+    }
+    console.log('Command-line input received:');
+    console.log('a: ' + result.a);
+    console.log('b: ' + result.b);
+    console.log('c: ' + result.c);
+
+    quad(result.a, result.b, result.c, function(err, quadsolve) {
+        if (err) {
+            console.log('Error: ', err);
+        } else {
+            console.log("Roots are " + quadsolve.root1() + "  " + quadsolve.root2());
+        }
+    });
 });
